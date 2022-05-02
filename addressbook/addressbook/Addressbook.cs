0330f1de50600c6a841a030b51fa03d4be039c2d
snehasonwane <snehasonwane01@gmail.com>
2022-05-02 23:33:33 +0530

    Address book deleting

--- addressbook/addressbook/Addressbook.cs
+++ addressbook/addressbook/Addressbook.cs
@@ -21,20 +21,84 @@
 //    }
 //}
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System;
 
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace AddressBookSolution
+//{
+//    public class AddressBook
+//    {
+//        List<Contact> addressBook = new List<Contact>();
+//        public AddressBook()
+//        {
+//            Contact address1 = new Contact()
+//            {
+//                FirstName = Console.ReadLine(),
+//                LastName = Console.ReadLine(),
+//                Address = Console.ReadLine(),
+//                State = Console.ReadLine(),
+//                EmailAddress = Console.ReadLine(),
+//                PostalCode = Convert.ToInt32(Console.ReadLine()),
+//                MobileNumber = Convert.ToInt64(Console.ReadLine())
+//            };
+
+//            addressBook.Add(address1);
+//        }
+//        public void AddContactToAddressBook(Contact contact)
+//        {
+//            addressBook.Add(contact);
+//        }
+//        public void Display()
+//        {
+//            foreach (var contact in addressBook)
+//            {
+//                Console.WriteLine(contact.FirstName + " " + contact.LastName + " " + contact.Address + " " + contact.State + " " + contact.EmailAddress + " " + " " + contact.PostalCode + " " + contact.MobileNumber);
+//            }
+//        }
+//        public void EditContactInAddressBook(string name)
+//        {
+//            foreach (var contact in addressBook)
+//            {
+//                if (contact.FirstName.Equals(name))
+//                {
+//                    Console.WriteLine("Enter the Option To Update");
+//                    Random random = new Random();
+//                    int option = random.Next(1, 7);
+//                    switch (option)
+//                    {
+//                        case 1:
+//                            Console.WriteLine("Enter the Information to Update");
+//                            contact.LastName = Console.ReadLine(); break;
+//                        case 2:
+//                            Console.WriteLine("Enter the Information to Update");
+//                            contact.Address = Console.ReadLine(); break;
+//                        case 3:
+//                            Console.WriteLine("Enter the Information to Update");
+//                            contact.State = Console.ReadLine(); break;
+//                        case 4:
+//                            Console.WriteLine("Enter the Information to Update");
+//                            contact.EmailAddress = Console.ReadLine(); break;
+//                        case 5:
+//                            Console.WriteLine("Enter the Information to Update");
+//                            contact.PostalCode = Convert.ToInt32(Console.ReadLine()); break;
+//                        case 6:
+//                            Console.WriteLine("Enter the Information to Update");
+//                            contact.MobileNumber = Convert.ToInt64(Console.ReadLine()); break;
+
+
+//                    }
+//                }
+//                Display();
+//            }
+//        }
+//    }
+//}
 
 namespace AddressBookSolution
 {
@@ -67,41 +131,18 @@ namespace AddressBookSolution
                 Console.WriteLine(contact.FirstName + " " + contact.LastName + " " + contact.Address + " " + contact.State + " " + contact.EmailAddress + " " + " " + contact.PostalCode + " " + contact.MobileNumber);
             }
         }
-        public void EditContactInAddressBook(string name)
+        public void DeletingContactINAddressBook(string name)
         {
+            Contact delete = new Contact();
             foreach (var contact in addressBook)
             {
                 if (contact.FirstName.Equals(name))
                 {
-                    Console.WriteLine("Enter the Option To Update");
-                    Random random = new Random();
-                    int option = random.Next(1, 7);
-                    switch (option)
-                    {
-                        case 1:
-                            Console.WriteLine("Enter the Information to Update");
-                            contact.LastName = Console.ReadLine(); break;
-                        case 2:
-                            Console.WriteLine("Enter the Information to Update");
-                            contact.Address = Console.ReadLine(); break;
-                        case 3:
-                            Console.WriteLine("Enter the Information to Update");
-                            contact.State = Console.ReadLine(); break;
-                        case 4:
-                            Console.WriteLine("Enter the Information to Update");
-                            contact.EmailAddress = Console.ReadLine(); break;
-                        case 5:
-                            Console.WriteLine("Enter the Information to Update");
-                            contact.PostalCode = Convert.ToInt32(Console.ReadLine()); break;
-                        case 6:
-                            Console.WriteLine("Enter the Information to Update");
-                            contact.MobileNumber = Convert.ToInt64(Console.ReadLine()); break;
-
-
-                    }
+                    delete = contact;
                 }
-                Display();
             }
+            addressBook.Remove(delete);
+            Display();
         }
     }
 }
--- addressbook/addressbook/Program.cs
+++ addressbook/addressbook/Program.cs
@@ -19,27 +19,28 @@
 //    }
 //}
 
-using System;
-using AddressBookSolution;
+//using System;
+//using AddressBookSolution;
+//class program
+//{
+//    public static void Main(String[] args)
+//    {
+//        Console.WriteLine("Enter the Address Details");
+//        AddressBook addressBook = new AddressBook();
+//        addressBook.Display();
+//    }
+//}
+
 class program
 {
     public static void Main(String[] args)
     {
         Console.WriteLine("Enter the Address Details");
-<<<<<<< Updated upstream
         AddressBook addressBook = new AddressBook();
         addressBook.Display();
-=======
-    Contact address=new Contact ();
-        address.FirstName =Console.ReadLine();
-        address.LastName =Console.ReadLine();
-        address.Address=Console.ReadLine();
-        address.State = Console.ReadLine();
-        address.EmailAddress = Console.ReadLine();
-        address.PostalCode =Convert.ToInt32(Console.ReadLine());
-        address.MobileNumber =Convert.ToInt64 (Console.ReadLine());
-        Console.WriteLine(address.FirstName+" "+ address.LastName+" "+ address.Address+" "+ address.State+" "+ address.EmailAddress+" "+ address.PostalCode+" "+ address.MobileNumber);
-        
->>>>>>> Stashed changes
+        Console.WriteLine("Enter the First Name");
+        string name = Console.ReadLine();
+        Console.WriteLine("Display after Deleting information ");
+        addressBook.DeletingContactINAddressBook(name);
     }
 }
