be6e3db779a9d0b5493696fe2bc8995f583c9927
snehasonwane <snehasonwane01@gmail.com>
2022-05-02 22:47:34 +0530

    Address book

---
+++ addressbook/addressbook/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using AddressBookSolution;
+class program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the Address Details");
+        AddressBook address = new AddressBook();
+        address.FirstName = Console.ReadLine();
+        address.LastName = Console.ReadLine();
+        address.Address = Console.ReadLine();
+        address.State = Console.ReadLine();
+        address.EmailAddress = Console.ReadLine();
+        address.PostalCode = Convert.ToInt32(Console.ReadLine());
+        address.MobileNumber = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine(address.FirstName + " " + address.LastName + " " + address.Address + " " + address.State + " " + address.EmailAddress + " " + address.PostalCode + " " + address.MobileNumber);
+
+    }
+}
